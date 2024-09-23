2509a146f717f61c8b9c99a6c52409893b6baabf
VsevolodKozlov-git <vsevolod026@gmail.com>
2024-09-23 08:30:10 +0300

    Добавил @ и поддержку typescript в eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,27 @@
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
+const path = require('node:path')
+const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting')
+
 module.exports = {
   root: true,
   extends: [
     'plugin:vue/vue3-essential',
-    '@vue/eslint-config-airbnb',
+    // '@vue/eslint-config-airbnb',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    // '@vue/eslint-config-typescript',
+    '@vue/eslint-config-airbnb-with-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'import/prefer-default-export': 'warn'
+  },
+  settings: {
+    ...createAliasSetting({
+      '@': `${path.resolve(__dirname, './src')}`
+    })
+  },
   overrides: [
     {
       files: [
