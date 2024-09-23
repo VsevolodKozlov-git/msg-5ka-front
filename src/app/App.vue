c3ea93997e4a08b626218d34f37b350f1f68dc91
VsevolodKozlov-git <vsevolod026@gmail.com>
2024-09-23 17:29:11 +0300

    test

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import { PlaygroundPage } from '@/pages'
+
+const tmp = 2
 </script>
 
 <template>
   <RouterView />
+
+  <div v-for="item in items" :key="item">item</div>
 </template>
 
 <style scoped></style>
